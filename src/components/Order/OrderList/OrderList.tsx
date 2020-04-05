012268f479b56145d07b5eda6593339a62aa3ec3
Lucas Eduardo <lucased78@hotmail.com>
2020-04-05 07:30:25 -0300

    [ADDED] OrderList component

---
+++ src/components/Order/OrderList/OrderList.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Order from '../Order/Order';
+import OrderModel from '../../../core/models/Order';
+import Divider from '../../Divider/Divider';
+
+export interface OrderListProps {
+  order: OrderModel | null
+}
+
+const OrderList = (props: OrderListProps) => {
+  return (
+    <>
+      {
+        props?.order?.tracks.reverse().map((e, i, arr) => {
+          return (
+            <React.Fragment key={Math.random()}>
+              <Order
+                title={e.status} 
+                location={e.locale} 
+                date={`${e.trackedAt.toLocaleDateString()} ás ${e.trackedAt.toLocaleTimeString()}`} />
+              
+              {i < arr.length - 1 ? <Divider width="70%" /> : null}
+            </React.Fragment>
+          )
+        })
+      }
+    </>
+  )
+}
+
+export default OrderList;
